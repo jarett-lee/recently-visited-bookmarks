891abd4b5ea6085c5acbede7fc7623e9eb706b2d
Owloid <jarett.lee.pi@gmail.com>
2017-01-25 23:00:43 -0500

    Display 2nd level bookmarks

--- options.js
+++ options.js
@@ -1,14 +1,55 @@
 
 // Saves options to chrome.storage
-function save_options() {
+function saveOptions() {
   // TODO
 }
 
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
-function restore_options() {
-  // TODO
+function restoreOptions() {
+  displayBookmarks(function() {
+  })
 }
 
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click', save_options);
+// Displays all the bookmarks the user currently has with a checkbox
+// to select the folders they want sorted.
+function displayBookmarks(callback) {
+  var display = document.getElementById('display');
+
+  // Working solution
+  chrome.bookmarks.getTree(function(results) {
+    var all_bookmarks = results[0].children;
+
+    // Start bookmarks list
+    var base_ul = document.createElement('UL');
+    display.appendChild(base_ul);
+
+    all_bookmarks.forEach(function(element) {
+      var bookmark_title = document.createTextNode(element.title);
+      var li = document.createElement('LI');
+      li.appendChild(bookmark_title);
+      base_ul.appendChild(li);
+
+      // Start sub list
+      var upper_ul = document.createElement('UL');
+      base_ul.appendChild(upper_ul);
+
+      element.children.forEach(function(element) {
+        var bookmark_title = document.createTextNode(element.title);
+        var li = document.createElement('LI');
+        li.appendChild(bookmark_title);
+        upper_ul.appendChild(li);
+
+      })
+
+    })
+    for (var i = 0; i < all_bookmarks.length; i++) {
+      all_bookmarks[i].children;
+    }
+  });
+
+  callback();
+}
+
+document.addEventListener('DOMContentLoaded', restoreOptions);
+document.getElementById('save').addEventListener('click', saveOptions);
