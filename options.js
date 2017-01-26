a3cd156c06d1b7edf893d62cc59a20e2049c3101
Owloid <jarett.lee.pi@gmail.com>
2017-01-26 11:08:14 -0500

    Add bookmark list expanding

--- options.js
+++ options.js
@@ -21,13 +21,13 @@ function displayBookmarks() {
         var root_ul = document.createElement('UL');
         display.appendChild(root_ul);
 
-        populateList(root_node, root_ul);
+        populateList(root_node, root_ul, 0);
 
     });
 }
 
-function populateList(node, list) {
-    var node_title = document.createTextNode(node.title);
+function populateList(node, list, num) {
+    var node_title = document.createTextNode(node.title + ' ' + num);
     var list_item = document.createElement('LI');
     list_item.appendChild(node_title);
     list.appendChild(list_item);
@@ -37,12 +37,26 @@ function populateList(node, list) {
 
         // Start a new list
         var child_list = document.createElement('UL');
+        child_list.classList.add('hide');
         list.appendChild(child_list);
 
-        node.children.forEach(function(chile_node) {
-            populateList(chile_node, child_list);
+        // Add expand on click
+        list_item.onmousedown = createExpander(child_list);
+
+        node.children.forEach(function(child_node, list_num) {
+            populateList(child_node, child_list, list_num);
         });
     }
+}
+
+function createExpander(child_list) {
+    return function() {
+        expandChildren(child_list);
+    }
+}
+
+function expandChildren(child_list) {
+    child_list.classList.toggle('hide');
 }
 
 document.addEventListener('DOMContentLoaded', displayBookmarks);
